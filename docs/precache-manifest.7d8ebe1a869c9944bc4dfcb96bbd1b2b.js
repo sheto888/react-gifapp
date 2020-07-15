f3b1d08dedc7a008ebe272c9c54668f5d3a3429f
israelthebest99@gmail.com <israelthebest99@gmail.com>
2020-07-15 10:03:12 -0500

    frist commit

---
+++ docs/precache-manifest.7d8ebe1a869c9944bc4dfcb96bbd1b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db03621f69301f42c235be26367629dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccb44c304ab0812086b4",
+    "url": "/static/css/main.2988c333.chunk.css"
+  },
+  {
+    "revision": "f6129525dbacbd7a8807",
+    "url": "/static/js/2.a52acf95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a52acf95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb44c304ab0812086b4",
+    "url": "/static/js/main.4936d14b.chunk.js"
+  },
+  {
+    "revision": "0cd9e99b5da4dbf30312",
+    "url": "/static/js/runtime-main.ca9e1759.js"
+  }
+]);
